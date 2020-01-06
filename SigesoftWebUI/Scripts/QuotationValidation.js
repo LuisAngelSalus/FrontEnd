fe030e31bea082b049661956be5255477d1a1945
AmcSalus <alberto.merchan@saluslaboris.com.pe>
2020-01-06 16:45:07 -0500

    validation Quotation

---
+++ SigesoftWebUI/Scripts/QuotationValidation.js
@@ -0,0 +1,32 @@
+﻿function ValidateQuotation(e) {
+    e.preventDefault();
+    //1)eliminar la clase error
+    clearErrors();
+    //Valida inputs
+    var validateRuc = validateInput("txtRuc", "Ruc requerido");
+    var validateFullName = validateInput("txtFullName", "Primer Contacto requerido");
+    var validateEmail = validateInput("txtEmail", "Email requerido");
+
+    //Retornar resultado de validación
+    if (validateRuc && validateFullName && validateEmail) {       
+        return true;
+    } else {   
+        return false;
+    }    
+}
+
+function validateInput(id, message) {
+    let element = document.getElementById(id);    
+    if (!element.checkValidity()) {       
+        newAlert(element, "VALIDACIÓN", message);
+        InputError(element);
+        return false;
+    }
+    return true;     
+}
+
+function clearErrors() {
+    $("#txtRuc").removeClass('error');
+    $("#txtFullName").removeClass('error');
+    $("#txtEmail").removeClass('error');
+}
