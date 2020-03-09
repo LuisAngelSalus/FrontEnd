37a4502d77ecd5510f94d75d8957e5bf5e5dd745
Pool Camacho <cgonzalesp@outlook.com>
2020-03-09 15:37:24 -0500

    update

--- BE/EmailDto.cs
+++ BE/EmailDto.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BE
 {
+    public class EmailSenderOptions
+    {
+        public int Port { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool EnableSsl { get; set; }
+        public string Name { get; set; }
+        public string Host { get; set; }
+    }
+
     public class EmailDto
     {
-        public string Email { get; set; }
-        public string Subject { get; set; }
-        public string Message { get; set; }
+        public string to { get; set; }
+        public string body { get; set; }
+        public string subject { get; set; }      
     }
+
+    
 }
--- SigesoftWebUI/Controllers/EmailController.cs
+++ SigesoftWebUI/Controllers/EmailController.cs
@@ -3,7 +3,9 @@ using BL;
 using SigesoftWebUI.Controllers.Base;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
@@ -13,11 +15,55 @@ namespace SigesoftWebUI.Controllers
     public class EmailController : GenericController
     {
         EmailBL _emailBL = new EmailBL();
+        private SmtpClient Cliente { get; }
+        private EmailSenderOptions Options { get; }
+        public EmailController()
+        {
+            Options = new EmailSenderOptions()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Password = "Aloha2019",
+                Email = "luis.delacruz@saluslaboris.com.pe",
+                Name = "Luis de la cruz",
+                EnableSsl = true,
+            };
+        }
         public JsonResult SendMail(EmailDto data)
         {
-            _emailBL.SendMail(data);
-            var response = "ok";
-            return Json(response, "application/json", Encoding.UTF8, JsonRequestBehavior.AllowGet);
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient(Options.Host);
+
+                string path = Path.Combine(HttpRuntime.AppDomainAppPath, "File");
+                string fileName = "PLANTILLA_PROPUESTA_COMERCIAL.docx";
+                string filePath = Path.Combine(path, fileName);
+
+                mail.From = new MailAddress(Options.Email, Options.Name, Encoding.UTF8);
+                mail.To.Add(data.to);
+                mail.Subject = data.subject;
+                mail.Body = data.body;
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment(filePath);
+                mail.Attachments.Add(attachment);
+
+                SmtpServer.Port = Options.Port;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(Options.Email, Options.Password);
+
+                SmtpServer.EnableSsl = Options.EnableSsl;
+
+                SmtpServer.Send(mail);
+
+                var response = "ok";
+
+                return Json(response, "application/json", Encoding.UTF8, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
