3b7d58e2291f0b41b014df016b590456abc9ca38
amc <albertomerchanc@hotmail.com>
2020-03-13 10:21:10 -0500

    schedule seleccionar protocol y limpiar tabla

--- SigesoftWebUI/Controllers/ScheduleController.cs
+++ SigesoftWebUI/Controllers/ScheduleController.cs
@@ -16,10 +16,21 @@ namespace SigesoftWebUI.Controllers
     {
         SecurityBL _securityBL = new SecurityBL();
         ScheduleBL _scheduleBL = new ScheduleBL();
-        
+        ProtocolBL _protocolBL = new ProtocolBL();
+
         public ActionResult Register()
         {
-            return View();
+            #region TOKEN
+            var sessione = (SessionModel)Session[Resources.Constante.SessionUsuario];
+            LoginDto oLoginDto = new LoginDto();
+            oLoginDto.v_UserName = sessione.UserName;
+            oLoginDto.v_Password = sessione.Pass;
+            var validated = _securityBL.ValidateAccess(oLoginDto);
+            if (validated == null) return Json("", "application/json", Encoding.UTF8, JsonRequestBehavior.AllowGet);
+            #endregion
+
+            ViewBag.PROTOCOLS = _protocolBL.GetProtocolsByCompanyId(sessione.CustomerCompanyId.Value, validated.Token);
+            return View();            
         }
 
         [HttpPost]
@@ -71,7 +82,7 @@ namespace SigesoftWebUI.Controllers
             var validated = _securityBL.ValidateAccess(oLoginDto);
             if (validated == null) return Json("", "application/json", Encoding.UTF8, JsonRequestBehavior.AllowGet);
             #endregion
-
+            
             var response = _scheduleBL.GetAdditionalComponents(protocolId, validated.Token);
             return Json(response, "application/json", Encoding.UTF8, JsonRequestBehavior.AllowGet);
         }
