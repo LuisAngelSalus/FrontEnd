16329c83c3614d0c7919ceb2bb57898b909e8000
Pool Camacho <cgonzalesp@outlook.com>
2020-03-25 11:43:42 -0500

    update

---
+++ SigesoftWebUI/Scripts/layout.js
@@ -0,0 +1,22 @@
+﻿var ajaxLoader = null;
+
+$(document).on("ready", function () {
+    ajaxLoader = $('body').loadingIndicator({
+        useImage: false,
+        showOnInit: false,
+        loadingText: ""
+    }).data("loadingIndicator");
+
+});
+
+function ShowLoading() {
+    if (ajaxLoader != null) {
+        ajaxLoader.show();
+    }
+}
+
+function CloseLoading() {
+    if (ajaxLoader != null) {
+        ajaxLoader.hide();
+    }
+}
