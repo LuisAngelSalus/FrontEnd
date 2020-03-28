9d63c847318ad3b5f9a661081d910186dadcbd1a
Pool Camacho <cgonzalesp@outlook.com>
2020-03-28 06:52:59 -0500

    update

--- BE/SystemUserDto.cs
+++ BE/SystemUserDto.cs
@@ -23,6 +23,7 @@ namespace BE
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public string Password { get; set; }
     }
 
     public class SystemUserRegisterDto
